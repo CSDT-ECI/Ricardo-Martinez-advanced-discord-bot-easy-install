fdb1090c4af58cb22a21dea441ab9fb7b587c277
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2024-03-25 06:06:33 +0000

    Change memes get

--- cmd/memes.js
+++ cmd/memes.js
@@ -1,14 +1,30 @@
-var settings = '../settingsConfig/settings.json';
-var file = require(settings)
-var memes = require('dankmemes');
+const axios = require('axios');
 
-exports.run = (bot, msg, params) => {
+async function getRandomMeme() {
+  try {
+    const response = await axios.get('https://www.reddit.com/r/memes/random/.json');
+    const post = response.data[0].data.children[0].data;
+    const meme = {
+      title: post.title,
+      image: post.url_overridden_by_dest,
+      subreddit: post.subreddit,
+      permalink: `https://reddit.com${post.permalink}`
+    };
+    return meme;
+  } catch (error) {
+    console.error('Error fetching meme:', error);
+    return null;
+  }
+}
 
-  memes('all', 100, function(err, data) {
-    var rand = Math.floor(Math.random() * 100);
-    var test = data[rand];
-    msg.channel.send(test);
-  });
+exports.run = async (bot, msg, params) => {
+
+  const meme = await getRandomMeme();
+  const embedMsg = new Discord.RichEmbed()
+    .setTitle(meme.title)
+    .setImage(meme.image)
+    .setColor("Blue");
+  msg.channel.send(embedMsg);
 
 };
 
