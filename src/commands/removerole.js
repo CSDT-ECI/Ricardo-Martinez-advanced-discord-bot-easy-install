0a81d08946f875074b372de06955b66500bc40ed
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2024-05-03 02:45:15 -0500

    remove unnecessary literal

--- src/commands/removerole.js
+++ src/commands/removerole.js
@@ -1,14 +1,14 @@
 exports.run = (bot, msg, params) => {
 
-    let member3 = msg.mentions.members.first();
-    if(!member3) return msg.reply(":x: " + "| You need to mention a user/member!");
-  
-    let muteRole3 = msg.mentions.roles.first();
-    if(!muteRole3) return msg.reply(":x: " + `| There is no such thing as a \"${muteRole3.name}\" role!`);
-  
-    member3.removeRole(muteRole3.id);
-    msg.channel.send(member3 + ` you have lost the role: ` + muteRole3.name + `!`);
-  
+  let member3 = msg.mentions.members.first();
+  if (!member3) return msg.reply(":x: " + "| You need to mention a user/member!");
+
+  let muteRole3 = msg.mentions.roles.first();
+  if (!muteRole3) return msg.reply(":x: " + `| There is no such thing as a "${muteRole3.name}" role!`);
+
+  member3.removeRole(muteRole3.id);
+  msg.channel.send(member3 + ` you have lost the role: ` + muteRole3.name + `!`);
+
 };
 
 exports.conf = {
